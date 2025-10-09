1605ffc9ced7e9cbc2fd4722c77065045708cbfc
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-09 14:04:14 -0500

    Inicio de decisiones

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -1,5 +1,11 @@
 public class Decisiones {
     public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
 
+        if (fechaDeLanzamiento > 2022) {
+            System.out.println("Peliculas más populares");
+        }
     }
 }
