852487b13c71468aed033f7072eeb6b82c3cf394
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-09 13:50:05 -0500

    Información de la primera pelicula Matrix

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ public class Main {
         System.out.println("Bienvenido(a) a Screen Match");
         System.out.println("Pelicula: Matrix");
 
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
 
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);
         }
     }
 }
