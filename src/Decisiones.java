1fd7a2003affcba74bcfa2333bcfabd638acfb06
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-09 17:14:31 -0500

    Inicio de la decisión 2

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -3,11 +3,16 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
+        String tipoPlan = "plus";
 
         if (fechaDeLanzamiento > 2022) {
             System.out.println("Peliculas más populares");
-        }else {
+        } else {
             System.out.println("Peliculas Retro que aún vale la pena ver");
+        }
+
+        if (incluidoEnElPlan || tipoPlan.equals("plus")) {
+            System.out.println("Disfrute de su pelicua");
         }
     }
 }
