b87006ecceb8d5426bb7f107c85ff5609406fd76
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-11 14:28:29 -0500

    Creación de void muestraFichaTecnica para la data de las peliculas

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,4 +3,10 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+
+    void muestraFichaTecnica(){
+        System.out.println("La pelicula se llama: " + nombre);
+        System.out.println("El tiempo de duración es: " + duracionEnMinutos);
+        System.out.println("Se estreno en: " + fechaDeLanzamiento);
+    }
 }
