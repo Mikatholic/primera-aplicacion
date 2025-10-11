a67101322ecebd207c3f5742b9343c609307af18
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-11 14:30:07 -0500

    Mejorando el código reemplazando los System.out.println() por .muestraFichaTecnica()

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,13 @@ public class Main {
         matrix1.duracionEnMinutos = 131;
         matrix1.fechaDeLanzamiento = 1999;
 
-        System.out.println("La pelicula es: " + matrix1.nombre);
-        System.out.println("El tiempo de duración es: " + matrix1.duracionEnMinutos);
-        System.out.println("Se estreno en: " + matrix1.fechaDeLanzamiento);
+        matrix1.muestraFichaTecnica();
 
         Pelicula matrix2 = new Pelicula();
         matrix2.nombre = "Matrix Recargado";
         matrix2.duracionEnMinutos = 138;
         matrix2.fechaDeLanzamiento = 2003;
 
-        System.out.println("La pelicula es: " + matrix2.nombre);
-        System.out.println("El tiempo de duración es: " + matrix2.duracionEnMinutos);
-        System.out.println("Se estreno en: " + matrix2.fechaDeLanzamiento);
+        matrix2.muestraFichaTecnica();
     }
 }
