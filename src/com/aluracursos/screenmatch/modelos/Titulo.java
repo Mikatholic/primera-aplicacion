84392c3f011365dae6d51feb9978a87cb3b30c5a
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-13 13:02:23 -0500

    Actualización del modelo con la data del modelo Pelicula.java

--- src/com/aluracursos/screenmatch/modelos/Titulo.java
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -1,13 +1,47 @@
 package com.aluracursos.screenmatch.modelos;
 
 public class Titulo {
-    private String director;
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
 
-    public String getDirector() {
-        return director;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public void setDirector(String director) {
-        this.director = director;
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
     }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDeLasEvaluaciones(){
+        return totalDeLasEvaluaciones;
+    }
+
+
+    public void muestraFichaTecnica() {
+        System.out.println("La pelicula se llama: " + nombre);
+        System.out.println("El tiempo de duración es: " + duracionEnMinutos);
+        System.out.println("Se estreno en: " + fechaDeLanzamiento);
+    }
+
+    public void evalua(double nota){
+        sumaDeLasEvaluaciones = sumaDeLasEvaluaciones + nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    public double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
+
 }
