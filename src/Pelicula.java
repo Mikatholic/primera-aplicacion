d236399e50afeed4c7c3395717d9b583dc5ac71a
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-13 11:38:06 -0500

    Implementación modificador de acceso private en double e int

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,8 +3,12 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
-    double sumaDeLasEvaluaciones;
-    int totalDeLasEvaluaciones;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
+
+    int getTotalDeLasEvaluaciones(){
+        return totalDeLasEvaluaciones;
+    }
 
 
     void muestraFichaTecnica() {
