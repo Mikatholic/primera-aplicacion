7a118051695e426e78ce9a4ff22be2e54c5a10c2
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-13 12:11:49 -0500

    Actualización e implementación de los setters

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
-package com.aluracursos.screenmatch.modelos;
+import com.aluracursos.screenmatch.modelos.Pelicula;
 
 public class Main {
     public static void main(String[] args) {
         Pelicula matrix1 = new Pelicula();
-        matrix1.nombre = "Matrix";
-        matrix1.duracionEnMinutos = 131;
-        matrix1.fechaDeLanzamiento = 1999;
+        matrix1.setNombre("Matrix");
+        matrix1.setDuracionEnMinutos(131);
+        matrix1.setFechaDeLanzamiento(1999);
+        matrix1.setIncluidoEnElPlan(true);
 
         matrix1.muestraFichaTecnica();
         matrix1.evalua(8.9);
