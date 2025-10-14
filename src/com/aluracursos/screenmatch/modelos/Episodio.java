bcaf1058278d2216cfb98b29231e5e9fd6a782e2
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-14 09:34:42 -0500

    Creación de atributos

--- src/com/aluracursos/screenmatch/modelos/Episodio.java
+++ src/com/aluracursos/screenmatch/modelos/Episodio.java
@@ -1,5 +1,31 @@
 package com.aluracursos.screenmatch.modelos;
 
 public class Episodio {
+    private int numero;
+    private String nombre;
+    private Serie serie;
 
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
