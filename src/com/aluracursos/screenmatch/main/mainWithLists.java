f3c0d80aca6a121e52b3eaf289ec0a5c614902cf
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-20 18:25:38 -0500

    Se copiaron las peliculas y la serie del Main.java

--- src/com/aluracursos/screenmatch/main/mainWithLists.java
+++ src/com/aluracursos/screenmatch/main/mainWithLists.java
@@ -1,4 +1,15 @@
 package com.aluracursos.screenmatch.main;
 
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
 public class mainWithLists {
+    public static void main(String[] args) {
+
+
+        Pelicula matrix1 = new Pelicula("Matrix", 1999);
+        var matrix2 = new Pelicula("Matrix Recargado", 2003);
+        var matrix3 = new Pelicula("Matrix Revoluciones", 2003);
+        Serie howIMeetYourMother = new Serie("How I meet your mother", 2005);
+    }
 }
