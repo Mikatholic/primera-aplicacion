645f1d9e6fd277a613b9d368e0a01a6dc3216f90
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-21 18:46:03 -0500

    Importando class List

--- src/com/aluracursos/screenmatch/main/mainWithLists.java
+++ src/com/aluracursos/screenmatch/main/mainWithLists.java
@@ -7,6 +7,7 @@ import com.aluracursos.screenmatch.modelos.Titulo;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class mainWithLists {
     public static void main(String[] args) {
