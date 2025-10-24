3aedecb00028b1f97f22c11bf8ce60c694f32737
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-24 10:23:24 -0500

    Agregando HttpRequest

--- src/com/aluracursos/screenmatch/main/MainWithSearch.java
+++ src/com/aluracursos/screenmatch/main/MainWithSearch.java
@@ -1,4 +1,11 @@
 package com.aluracursos.screenmatch.main;
 
 public class MainWithSearch {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://foo.com/"))
+                .build();
+    }
 }
