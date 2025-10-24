df2161dbca10b80e8e065c54a2cf920c67d7a359
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-24 11:19:25 -0500

    Agregando Scanner

--- src/com/aluracursos/screenmatch/main/MainWithSearch.java
+++ src/com/aluracursos/screenmatch/main/MainWithSearch.java
@@ -5,9 +5,12 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
@@ -16,6 +19,6 @@ public class MainWithSearch {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-
+        System.out.println(response.body());
     }
 }
