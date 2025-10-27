37e21e007484d88a6169d50adbacdf8ae926366b
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-27 09:49:39 -0500

    Creando la classe para mi propia excepción

---
+++ src/com/aluracursos/screenmatch/excepcion/ErrorEnConversionDeDuracionException.java
@@ -0,0 +1,6 @@
+package com.aluracursos.screenmatch.excepcion;
+
+public class ErrorEnConversionDeDuracionException extends Throwable {
+    public ErrorEnConversionDeDuracionException(String s) {
+    }
+}
--- src/com/aluracursos/screenmatch/modelos/Titulo.java
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch.modelos;
 
+import com.aluracursos.screenmatch.excepcion.ErrorEnConversionDeDuracionException;
 import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable<Titulo>{
@@ -21,7 +22,8 @@ public class Titulo implements Comparable<Titulo>{
         this.nombre = miTituloOmdb.title();
         this.fechaDeLanzamiento = Integer.valueOf(miTituloOmdb.year());
         if (miTituloOmdb.runtime().contains("N/A")){
-
+                throw new ErrorEnConversionDeDuracionException("No pude convertir la duración," +
+                        "porque contiene un N/A");
         }
         this.duracionEnMinutos = Integer.valueOf(
                 miTituloOmdb.runtime().substring(0,3).replace(" ", ""));
