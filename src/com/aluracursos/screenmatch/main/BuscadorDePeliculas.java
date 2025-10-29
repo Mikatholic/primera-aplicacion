9b30863b306fe526943343c5952e18a2a705e39b
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-29 18:31:56 -0500

    Creando string con la URL

--- src/com/aluracursos/screenmatch/main/BuscadorDePeliculas.java
+++ src/com/aluracursos/screenmatch/main/BuscadorDePeliculas.java
@@ -2,5 +2,5 @@ package com.aluracursos.screenmatch.main;
 
 public class BuscadorDePeliculas {
     private static final String API_KEY = "ba29c8e"; // Se mantienen la clave en un lugar centralizado
-
+    private static final String BASE_URL = "https://www.omdbapi.com/?t=";
 }
