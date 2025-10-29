6629679d52a3bb6c42ef8169b011bd0d3bf4d5e1
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-29 18:44:11 -0500

    Importando clase GsonBuilder

--- src/com/aluracursos/screenmatch/main/AdministradorGson.java
+++ src/com/aluracursos/screenmatch/main/AdministradorGson.java
@@ -1,9 +1,11 @@
 package com.aluracursos.screenmatch.main;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class AdministradorGson {
     public Gson obtenerGson(){
-
+return new GsonBuilder()
+        .
     }
 }
