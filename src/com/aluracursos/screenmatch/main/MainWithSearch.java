112175afaf2359aff6bc534499a7c401f6f4e132
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-29 18:00:59 -0500

    Moviendo el Gson

--- src/com/aluracursos/screenmatch/main/MainWithSearch.java
+++ src/com/aluracursos/screenmatch/main/MainWithSearch.java
@@ -21,13 +21,13 @@ public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner lectura = new Scanner(System.in);
         List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
 
         while(true){
             System.out.println("Escriba el nombre de una pelicula: ");
             var busqueda = lectura.next();
-            Gson gson = new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                    .create();
 
             if(busqueda.equalsIgnoreCase("salir")){
                 break;
@@ -61,7 +61,7 @@ public class MainWithSearch {
         }
         System.out.println(titulos);
         FileWriter escritura = new FileWriter("titulos.json")
-                escritura.write();
+                escritura.write(gson.toJson());
         System.out.println("Finalizo la ejecución del programa");
 
     }
