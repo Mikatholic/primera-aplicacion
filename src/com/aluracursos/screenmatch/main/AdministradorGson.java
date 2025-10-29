8844c383147a94d9250b87c4778359005d759d4a
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-29 18:49:13 -0500

    Importando List

--- src/com/aluracursos/screenmatch/main/AdministradorGson.java
+++ src/com/aluracursos/screenmatch/main/AdministradorGson.java
@@ -4,6 +4,7 @@ import com.aluracursos.screenmatch.modelos.Titulo;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.util.List;
 
 public class AdministradorGson {
     public Gson obtenerGson(){
