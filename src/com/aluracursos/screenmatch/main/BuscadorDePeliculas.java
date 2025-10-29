475e817192ce9aa03fe2cfea7ad056c9ecf8438d
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-29 18:30:51 -0500

    Creando string con API_KEY

--- src/com/aluracursos/screenmatch/main/BuscadorDePeliculas.java
+++ src/com/aluracursos/screenmatch/main/BuscadorDePeliculas.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch.main;
 
 public class BuscadorDePeliculas {
+    private static final String API_KEY = "ba29c8e"; // Se mantienen la clave en un lugar centralizado
 
 }
