4b073c010543184e69bbf9935b1ec0ffa6324802
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-29 18:47:27 -0500

    Importando clase Titulo

--- src/com/aluracursos/screenmatch/main/AdministradorGson.java
+++ src/com/aluracursos/screenmatch/main/AdministradorGson.java
@@ -1,11 +1,16 @@
 package com.aluracursos.screenmatch.main;
 
+import com.aluracursos.screenmatch.modelos.Titulo;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class AdministradorGson {
     public Gson obtenerGson(){
-return new GsonBuilder()
-        .
+    return new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+            .setPrettyPrinting()
+            .create();
     }
+    public String convertirAJson(List<Titulo> titulos)
 }
