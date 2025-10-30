47127643b55027025b0f325c9135feddf5f007f8
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-29 19:03:53 -0500

    Finalizaciòn clase EscrituraDeDatos

--- src/com/aluracursos/screenmatch/main/EscrituraDeDatos.java
+++ src/com/aluracursos/screenmatch/main/EscrituraDeDatos.java
@@ -1,5 +1,20 @@
 package com.aluracursos.screenmatch.main;
 
+import com.aluracursos.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class EscrituraDeDatos {
 
+    public void guardarTitulos(List<Titulo> titulos, Gson gson) throws IOException{
+        String jsonParaGuardar = gson.toJson(titulos);
+
+        // El nombre del archivo puede ser un parámetro o una constante.
+        try (FileWriter escritura = new FileWriter("titulos.json")) {
+            escritura.write(jsonParaGuardar);
+        } // El 'try-with-resources' asegura que 'escritura.close()' se llame automáticamente.
+    }
 }
--- src/com/aluracursos/screenmatch/main/MainWithSearch.java
+++ src/com/aluracursos/screenmatch/main/MainWithSearch.java
@@ -3,16 +3,8 @@ package com.aluracursos.screenmatch.main;
 import com.aluracursos.screenmatch.excepcion.ErrorEnConversionDeDuracionException;
 import com.aluracursos.screenmatch.modelos.Titulo;
 import com.aluracursos.screenmatch.modelos.TituloOmdb;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -21,10 +13,6 @@ public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner lectura = new Scanner(System.in);
         List<Titulo> titulos = new ArrayList<>();
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .setPrettyPrinting()
-                .create();
 
         while(true){
             System.out.println("Escriba el nombre de una pelicula: ");
@@ -37,8 +25,6 @@ public class MainWithSearch {
             try{
 
 
-                String json = response.body();
-                System.out.println(response.body());
 
                 TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
                 System.out.println(miTituloOmdb);
@@ -55,9 +41,7 @@ public class MainWithSearch {
             }
         }
         System.out.println(titulos);
-        FileWriter escritura = new FileWriter("titulos.json");
-                escritura.write(gson.toJson(titulos));
-                escritura.close();
+
         System.out.println("Finalizo la ejecución del programa");
 
     }
